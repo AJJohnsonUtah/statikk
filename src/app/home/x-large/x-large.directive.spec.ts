eefc2673549333b8054efc82f910fa3f6c7e6da1
AJ.Johnson@utah.edu <AJ.Johnson@utah.edu>
2017-03-29 08:35:16 -0600

    Upgrading Angular version to 4.0.0

--- src/app/home/x-large/x-large.directive.spec.ts
+++ src/app/home/x-large/x-large.directive.spec.ts
@@ -6,8 +6,8 @@ import {
 } from '@angular/core/testing';
 import { Component } from '@angular/core';
 import { BaseRequestOptions, Http } from '@angular/http';
-import { By } from '@angular/platform-browser/src/dom/debug/by';
 import { MockBackend } from '@angular/http/testing';
+import { By } from '@angular/platform-browser';
 
 // Load the implementations that should be tested
 import { XLargeDirective } from './x-large.directive';
