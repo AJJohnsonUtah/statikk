e9f40be192ba84f48d1099573ea1a355f53501dd
AJ.Johnson@utah.edu <AJ.Johnson@utah.edu>
2017-04-07 15:22:54 -0600

    Adding a test suite and ammending controller to return JSON data

--- src/main/java/com/njin/loltheoryapi/controller/SummonerDataController.java
+++ src/main/java/com/njin/loltheoryapi/controller/SummonerDataController.java
@@ -5,16 +5,15 @@
  */
 package com.njin.loltheoryapi.controller;
 
-import com.njin.loltheory.riotapi.model.ChampionMastery;
 import com.njin.loltheory.riotapi.model.Region;
 import com.njin.loltheory.riotapi.service.RiotApiService;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,34 +28,50 @@ public class SummonerDataController {
     @Autowired
     RiotApiService riotApiService;
 
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/champion-mastery/all/{region}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getChampionMastery(@PathVariable("summonerId") long summonerId, @PathVariable("region") String region) {
         return riotApiService.getChampionMastery(Region.valueOf(region), summonerId);
     }
 
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/champion-mastery/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getChampionMastery(@PathVariable("summonerId") long summonerId) {
         return riotApiService.getChampionMastery(Region.NA, summonerId);
     }
 
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/champion-mastery/top/{region}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getTopChampionMastery(@PathVariable("summonerId") long summonerId, @PathVariable("region") String region) {
         return riotApiService.getTopChampionMastery(Region.valueOf(region), summonerId);
     }
 
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/champion-mastery/top", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getTopChampionMastery(@PathVariable("summonerId") long summonerId) {
         return riotApiService.getTopChampionMastery(Region.NA, summonerId);
     }
-    
+
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/current-game/{region}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCurrentGame(@PathVariable("summonerId") long summonerId, @PathVariable("region") String region) {
-        return riotApiService.getCurrentGame(Region.valueOf(region), summonerId);
+        String response = riotApiService.getCurrentGame(Region.valueOf(region), summonerId);
+        if (response == null) {
+            return "{}";
+        } else {
+            return response;
+        }
     }
 
+    @ResponseBody
     @RequestMapping(value = "/{summonerId}/current-game", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCurrentGame(@PathVariable("summonerId") long summonerId) {
-        return riotApiService.getCurrentGame(Region.NA, summonerId);
-    }    
+        String response = riotApiService.getCurrentGame(Region.NA, summonerId);
+        if (response == null) {
+            return "{}";
+        } else {
+            return response;
+        }
+    }
 
 }
