c09cd2ac85297a254eb541fb4baa09b635645a78
AJ Johnson <AJJohnsonUtah@gmail.com>
2017-11-16 17:53:13 -0700

    Updating front-end to have working tests

--- web-app/src/app/core/services/summoner-data.service.ts
+++ web-app/src/app/core/services/summoner-data.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/toPromise';
 import { SummonerDto } from '../../shared/models/riot-api-types/summoner-dto';
 import { MatchlistDto } from '../../shared/models/riot-api-types/matchlist-dto';
 import { HttpService } from './http.service';
