7e88a1c61c4925dbb5aea39fcce2512b85d1e814
AJ Johnson <AJJohnsonUtah@gmail.com>
2017-11-18 12:07:36 -0700

    Updating web-app to run headless tests in Chrome

--- web-app/src/app/app.component.spec.ts
+++ web-app/src/app/app.component.spec.ts
@@ -1,24 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { StaticDataService } from './core/services/static-data.service';
 describe('AppComponent', () => {
+
+  class StaticDataServiceStub {
+    loadRealmsData() { }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [{ provide: StaticDataService, useClass: StaticDataServiceStub }]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
   }));
   it('should render title in a div with id "main-logo"', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
