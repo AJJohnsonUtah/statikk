f70ef5a791de194d4b5fa666791b5f082b88e2e1
AJ Johnson <AJJohnsonUtah@gmail.com>
2017-12-22 16:09:46 -0700

    Adding basic button(s) to identify user in team builder

---
+++ web-app/src/app/features/team-builder/models/champion-pick.ts
@@ -0,0 +1,4 @@
+export interface ChampionPick {
+    championId: number;
+    summonerName: string;
+}
--- web-app/src/app/features/team-builder/team-builder.component.ts
+++ web-app/src/app/features/team-builder/team-builder.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { StaticDataService } from '../../core/services/static-data.service';
 import { StaticChampion } from '../../shared/models/riot-api-types/static-champion';
-
+import { ChampionPick } from './models/champion-pick';
 @Component({
   selector: 'app-team-builder',
   templateUrl: './team-builder.component.html',
@@ -11,18 +12,32 @@ export class TeamBuilderComponent implements OnInit {
 
   playersPerTeam = 5;
   currentPick = 0;
-  teamAChampions: number[];
-  teamBChampions: number[];
+  teamAChampions: ChampionPick[] = [];
+  teamBChampions: ChampionPick[] = [];
+  firstPickTeam: ChampionPick[] = [];
+  lastPickTeam: ChampionPick[];
+  userPick: ChampionPick;
+
   staticChampionIds: string[] = [];
+
+  @ViewChild('t') public tooltip: NgbTooltip;
 
   constructor(private staticDataService: StaticDataService) { }
 
   ngOnInit() {
+    this.tooltip.open();
     this.staticDataService.getChampions().subscribe((staticChampions) => {
       this.staticChampionIds = Object.keys(staticChampions);
     });
-    this.teamAChampions = new Array<number>(5);
-    this.teamBChampions = new Array<number>(5);
+    for (let i = 0; i < this.playersPerTeam; i++) {
+      this.teamAChampions.push({ championId: null, summonerName: null });
+      this.teamBChampions.push({ championId: null, summonerName: null });
+    }
+  }
+
+  selectUserPick(pick: ChampionPick) {
+    this.userPick = pick;
+    this.tooltip.close();
   }
 
 }
